e41c66957eeeed1fd30c1b1bbf468f529d7c1a28
Nathan N <Nitharios@users.noreply.github.com>
2017-10-03 11:51:06 -1000

    Based client established

--- client.js
+++ client.js
@@ -3,5 +3,16 @@ let sanity = "You're not crazy!";
 console.log(sanity);
 
 const net = require('net');
+const PORT = process.env.PORT || 8080;
 
-const request = net.connection
+const request = new net.connect(PORT, () => {
+  console.log(`Connected to server at port ${PORT}`);
+});
+
+request.on('error', (err) => {
+  throw err;
+});
+
+request.on('end', () => {
+  console.log('Disconnected from server');
+});
