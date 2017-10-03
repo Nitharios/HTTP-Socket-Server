74681bf0e89187f6338c43d8f4864e517a7c542e
Nathan N <Nitharios@users.noreply.github.com>
2017-10-02 15:57:31 -1000

    Header format fixed

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const sanity = "You're not crazy!";
 console.log('1st:', sanity);
 
+const timeStamp = new Date();
+
 let requestData = [];
 
 ///// ACTUAL CODE /////
@@ -42,20 +44,16 @@ server.listen(PORT, () => {
 function formatData(data) {
   let tempData = data.split('\r\n');
   // console.log(tempData);
-  
-  console.log(tempData[0]);
-  // request.write(tempData[0]);
-  console.log(tempData[5]);
-  let time = new Date();
-  console.log('Date:', time);
-  console.log(serverName);
+  let header = 
+    `${tempData[0]}\n${tempData[5]}\nTime Stamp: ${timeStamp}\n${serverName}`;
+
+  console.log(header);
+  // time = moment(time).format('MMMM Do YYYY, h:mm:ss a');
 }
 
+///// SCRATCH /////
 
-
-
-
-// cojst server = http.createServer((request, response) => {
+// const server = http.createServer((request, response) => {
 //   // request = setEncoding('utf8');
 //   // sent twice?? two requests??? ASYNC...
 //   console.log('Server: Someone has connected!');
