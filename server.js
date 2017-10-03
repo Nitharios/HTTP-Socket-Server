e49e2b061aa7ff7367565baf89bdd2b42b24ee74
Nathan N <Nitharios@users.noreply.github.com>
2017-10-02 15:36:27 -1000

    Started formatting returned data

--- server.js
+++ server.js
@@ -3,11 +3,14 @@
 const sanity = "You're not crazy!";
 console.log('1st:', sanity);
 
+let requestData = [];
+
 ///// ACTUAL CODE /////
 
 const net = require('net');
 const fs = require('fs');
 // const http = require('http');
+const serverName = 'https://www.ihateNET.com';
 const PORT = process.env.PORT || 8080;
 
 const server = net.createServer((request) => {
@@ -15,10 +18,8 @@ const server = net.createServer((request) => {
 
   console.log('2nd:', sanity);
 
-  // request.write(sanity);
-
   request.on('data', (data) => {
-    console.log('Client has emitted:\n' + data );
+    formatData(data);
   });
 
   request.end();
@@ -36,6 +37,24 @@ server.on('error', (err) => {
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+///// FUNCTIONS /////
+function formatData(data) {
+  let tempData = data.split('\r\n');
+  // console.log(tempData);
+  
+  console.log(tempData[0]);
+  // request.write(tempData[0]);
+  console.log(tempData[5]);
+  let time = new Date();
+  console.log('Date:', time);
+  console.log(serverName);
+}
+
+
+
+
+
 // cojst server = http.createServer((request, response) => {
 //   // request = setEncoding('utf8');
 //   // sent twice?? two requests??? ASYNC...
