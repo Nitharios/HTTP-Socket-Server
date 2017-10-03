80750421ea8d97e7b78513133855385969261832
Nathan N <Nitharios@users.noreply.github.com>
2017-10-02 14:12:58 -1000

    Server up, passes console and front-end sanity checks

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 // jshint esversion:6
 
 const sanity = "You're not crazy!";
-console.log(sanity);
+console.log('1st:', sanity);
+
+///// ACTUAL CODE /////
 
 const http = require('http');
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer((request, res) => {
+  // client = setEncoding('utf8');
+  // sent twice?? two requests???
+  console.log('HTTP:', sanity);
+  res.writeHead(200, {"Content-Type" : "text/HTML"});
+  res.write(`${sanity} \r\n`);
+  res.end();
+
+});
+
+server.on('error', (err) => {
+  throw err;
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
