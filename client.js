1a7ca8d8cf99bd76f83c8e9a2670e150ef2210f8
Nathan N <Nitharios@users.noreply.github.com>
2017-10-03 13:39:16 -1000

    Able to catch arguments from console

--- client.js
+++ client.js
@@ -4,10 +4,15 @@ console.log(sanity);
 
 const net = require('net');
 const PORT = process.env.PORT || 8080;
+let url = process.argv[2].split('/')[0];
+let uri = process.argv[2].split('/')[1];
 
 const request = new net.connect(PORT, () => {
   console.log(`Connected to server at port ${PORT}`);
   console.log(process.argv);
+
+  console.log(url);
+  console.log(uri);
 
   process.stdin.pipe(request);
 
