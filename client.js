cf94d78ee7ad047cd6f0167c87745c2345f08d4c
Nathan N <Nitharios@users.noreply.github.com>
2017-10-03 16:31:51 -1000

    Method for handling flags added

--- client.js
+++ client.js
@@ -5,19 +5,18 @@ console.log(sanity);
 const net = require('net');
 const PORT = process.env.PORT || 8080;
 const userAgent = 'nathan/1.1';
-const method = 'GET';
 const timeStamp = new Date();
 
 // returns an array
 let commandLineInput = process.argv;
-let url, uri, flag;
+let url, uri, flag, method;
 
 const request = new net.connect(PORT, () => {
   console.log(`Connected to server at port ${PORT}`);
-  console.log('process', process.argv);
+  // console.log('process', process.argv);
 
   // will send the request to the server
-  // commandHandler(commandLineInput);
+  commandHandler(commandLineInput);
   generateRequest(request);
 
   process.stdin.pipe(request);
@@ -35,18 +34,35 @@ request.on('end', () => {
 
 /* FUNCTIONS */
 
-// deals with command line input
+// deals with command line input, parameter is an array
 function commandHandler(input) {
-  // first input should be node directory
-  // second input should be the file to access
-  // third input COULD be a flag or link
-  // fourth input COULD be a link or null
+  // zero index should be node directory
+  // first index should be access file
+  // second index COULD be a flag or link
+  // third index COULD be a link or null
+  if (input[2] === '-I') {
+      method = 'HEAD';
+      
+      if (input[3].toLowerCase().includes('www')) {
+        url = input[3].split('/')[0];
+        uri = input[3].split('/')[1];
+      }
+
+  } else if (input[2].toLowerCase().includes('www')) {
+    method = 'GET';
+    url = input[2].split('/')[0];
+    uri = input[2].split('/')[1];
+  } 
 }
 
 
 // generates the Request Header
-function generateRequest(request) {
-  request.write(`${method} /${uri} HTTP/1.1\nDate:${timeStamp}\nHost:${url}:${PORT}\nUser-Agent: ${userAgent}\nAccept:*/*`);
+function generateRequest(request, method) {
+  request.write(`${method} /${uri} HTTP/1.1\nDate:${timeStamp}\nHost:${url}:${PORT}\nUser-Agent: ${userAgent}\nAccept:*/*`, (err) => {
+    if (err) throw err;
+
+    request.end();
+  });
 }
 
 // generates the Request Body
