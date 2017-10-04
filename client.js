10d8fcb548a52fb171e490e83680ab9ddc57be5b
Nathan N <Nitharios@users.noreply.github.com>
2017-10-03 14:41:01 -1000

    Client able to process correct data from server

--- client.js
+++ client.js
@@ -4,21 +4,22 @@ console.log(sanity);
 
 const net = require('net');
 const PORT = process.env.PORT || 8080;
+const userAgent = 'nathan/1.1';
+const method = 'GET';
+
 let url = process.argv[2].split('/')[0];
 let uri = process.argv[2].split('/')[1];
 
 const request = new net.connect(PORT, () => {
   console.log(`Connected to server at port ${PORT}`);
-  console.log(process.argv);
+  console.log('process', process.argv);
 
-  console.log(url);
-  console.log(uri);
+  // will send the request to the server
+  generateRequest(request);
 
   process.stdin.pipe(request);
-
   request.pipe(process.stdout);
 
-  request.end();
 });
 
 request.on('error', (err) => {
@@ -26,5 +27,20 @@ request.on('error', (err) => {
 });
 
 request.on('end', () => {
-  console.log('Disconnected from server');
+  console.log('\nDisconnected from server');
+});
+
+/* FUNCTIONS */
+
+// generates the Request Header
+function generateRequest(request) {
+  request.write(`${method} /${uri} HTTP/1.1\nHost:${url}:${PORT}\nUser-Agent: ${userAgent}\nAccept:*/*`);
+}
+
+// generates the Request Body
+
+
+// reads in data sent from server
+request.on('data', (data) => {
+  request.end();
 });
