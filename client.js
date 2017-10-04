13ae4d5d42bcaa09915bac76aedaaae2d2195407
Nathan N <Nitharios@users.noreply.github.com>
2017-10-04 12:17:35 -1000

    Cleaned up code

--- client.js
+++ client.js
@@ -6,29 +6,26 @@ console.log(sanity);
 
 const net = require('net');
 const IP = process.env.IP || 'localhost';
-// console.log('IP', IP);
-const PORT = process.env.PORT || 8080;
 const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36';
 const accept = 'text/html, application/json';
 const connection = 'keep-alive';
 
 const howTo = `\nHELP\n
-  node [fileName] [,option] [path]\n\nOPTIONS\n
+  node [fileName] [,option] [www.DEFINEPATH.extension]\n\nOPTIONS\n
   -I fetch header
         Returns the header of the requested path; if left out, the body of path will be returned\n\n`;
 
 // returns an array
 let host, uri, flag, method;
+let PORT = process.env.PORT || 80;
 let commandLineInput = process.argv;
 
 commandHandler(commandLineInput);
 
 const request = new net.connect({port: PORT, host: host}, () => {
   console.log(`Connected to server at port ${PORT}`);
-  // console.log('process', process.argv);
 
   // will send the request to the server
-  // commandHandler(commandLineInput);
   generateRequest(request, method);
 
   // process.stdin.pipe(request);
@@ -45,9 +42,15 @@ request.on('end', () => {
 
 // reads in data sent from server
 request.on('data', (data) => {
-  // console.log(data.toString());
   let serverReply = data.toString();
-  if (method === 'GET') serverReply = serverReply.slice(serverReply.indexOf('\n\n')+1, serverReply.length-1).trim();
+
+  if (method === 'GET') {
+    serverReply = serverReply
+    .slice(serverReply
+    .indexOf('\n\n')+1, serverReply.length-1)
+    .trim();
+  }
+
   process.stdin.write(serverReply);
   request.end();
 });
@@ -61,26 +64,35 @@ function commandHandler(input) {
   // second index COULD be a flag or link
   // third index COULD be a link or null
   // console.log('input', input);
+  input.forEach(setPort, input);
+
   if (input[2] === '-I') {
       method = 'HEAD';
       
-      if (input[3].toLowerCase().includes('www') || input[3].toLowerCase().includes('localhost')) {
+      if (input[3].toLowerCase().includes('www') || 
+        input[3].toLowerCase().includes('localhost')) {
+
         host = input[3].split('/')[0];
-        // console.log(host);
         uri = input[3].split('/')[1] || '';
       }
 
-  } else if (input[2].toLowerCase().includes('www') || input[2].toLowerCase().includes('localhost')) {
-    console.log('here');
+  } else if (input[2].toLowerCase().includes('www') || 
+    input[2].toLowerCase().includes('localhost')) {
     method = 'GET';
     host = input[2].split('/')[0] || input[2];
-    // console.log('host', host);
     uri = input[2].split('/')[1] || '';
   
   } else {
     method = 'GET';
     process.stdin.write(howTo);
   } 
+}
+
+function setPort(current, index) {
+  if (current.includes(':')) {
+    PORT = current.slice(current.indexOf(':')+1, current.length);
+    this[index] = current.slice(0, current.indexOf(':'));
+  }
 }
 
 // generates the Request Header
